c8484888ee5693c15c55cc28c2ad71f4878caf0e
Eszter <steszti@gmail.com>
2020-07-08 10:17:31 +0200

    feat: Init crypto data component with table in it

---
+++ src/app/crypto-data/crypto-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-data',
+  templateUrl: './crypto-data.component.html',
+  styleUrls: ['./crypto-data.component.css']
+})
+export class CryptoDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
