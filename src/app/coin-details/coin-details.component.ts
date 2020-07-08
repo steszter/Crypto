90510452f086a5e8ed7ed2af11bc798f744a6290
Eszter <steszti@gmail.com>
2020-07-08 15:07:10 +0200

    feat: Get param id, get json data, show data based on id

--- src/app/coin-details/coin-details.component.ts
+++ src/app/coin-details/coin-details.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-coin-details',
   templateUrl: './coin-details.component.html',
-  styleUrls: ['./coin-details.component.css']
+  styleUrls: ['./coin-details.component.css'],
 })
 export class CoinDetailsComponent implements OnInit {
+  API_URL = 'https://api.coinlore.net/api/ticker/?id=';
+  coinId;
+  coinDetails = [];
+  objectKeys = Object.keys;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private apiService: ApiService) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((parameter) => {
+      this.coinId = parameter.id;
+    });
+    this.apiService.sendGetRequest(this.API_URL + this.coinId).subscribe(
+      (data: any[]) => {
+        this.coinDetails = data;
+        console.log(this.coinDetails);
+      },
+      (error) => console.log(error)
+    );
   }
-
 }
