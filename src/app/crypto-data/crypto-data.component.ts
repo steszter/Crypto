a0b0dbd9695ef41de3d8f00a5234c010c8a389a2
Eszter <steszti@gmail.com>
2020-07-08 13:55:45 +0200

    feat: Add info of crypto market

--- src/app/crypto-data/crypto-data.component.ts
+++ src/app/crypto-data/crypto-data.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-crypto-data',
   templateUrl: './crypto-data.component.html',
-  styleUrls: ['./crypto-data.component.css']
+  styleUrls: ['./crypto-data.component.css'],
 })
 export class CryptoDataComponent implements OnInit {
+  cryptoDataGlobal = [];
+  objectKeys = Object.keys;
+  API_URL = 'https://api.coinlore.net/api/global/';
 
-  constructor() { }
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
+    this.apiService.sendGetRequest(this.API_URL).subscribe(
+      (data: string[]) => {
+        this.cryptoDataGlobal = data;
+        console.log('final:', this.cryptoDataGlobal);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
