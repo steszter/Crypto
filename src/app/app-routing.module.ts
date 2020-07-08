dd6d7b1ad7171aa585c0d2f71f2133628c2ba5fc
Eszter <steszti@gmail.com>
2020-07-08 10:18:33 +0200

    feat: Add routes, imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CryptoDataComponent } from './crypto-data/crypto-data.component';
+import { CoinDetailsComponent } from './coin-details/coin-details.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'crypto', component: CryptoDataComponent },
+  { path: 'coin/:id', component: CoinDetailsComponent },
+  { path: '', redirectTo: '/crypto', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CryptoDataComponent } from './crypto-data/crypto-data.component';
+import { TableComponent } from './table/table.component';
+import { CoinDetailsComponent } from './coin-details/coin-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CryptoDataComponent,
+    TableComponent,
+    CoinDetailsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
