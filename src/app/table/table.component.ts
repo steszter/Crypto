cc0c82e445beb57d93d205b4a3a5695c21bf5f32
Eszter <steszti@gmail.com>
2020-07-08 13:56:06 +0200

    feat: Add coins data to table

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,94 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-interface Country {
-  id?: number;
-  name: string;
-  flag: string;
-  area: number;
-  population: number;
-}
-
-const COUNTRIES: Country[] = [
-  {
-    name: 'Russia',
-    flag: 'f/f3/Flag_of_Russia.svg',
-    area: 17075200,
-    population: 146989754,
-  },
-  {
-    name: 'France',
-    flag: 'c/c3/Flag_of_France.svg',
-    area: 640679,
-    population: 64979548,
-  },
-  {
-    name: 'Germany',
-    flag: 'b/ba/Flag_of_Germany.svg',
-    area: 357114,
-    population: 82114224,
-  },
-  {
-    name: 'Portugal',
-    flag: '5/5c/Flag_of_Portugal.svg',
-    area: 92090,
-    population: 10329506,
-  },
-  {
-    name: 'Canada',
-    flag: 'c/cf/Flag_of_Canada.svg',
-    area: 9976140,
-    population: 36624199,
-  },
-  {
-    name: 'Vietnam',
-    flag: '2/21/Flag_of_Vietnam.svg',
-    area: 331212,
-    population: 95540800,
-  },
-  {
-    name: 'Brazil',
-    flag: '0/05/Flag_of_Brazil.svg',
-    area: 8515767,
-    population: 209288278,
-  },
-  {
-    name: 'Mexico',
-    flag: 'f/fc/Flag_of_Mexico.svg',
-    area: 1964375,
-    population: 129163276,
-  },
-  {
-    name: 'United States',
-    flag: 'a/a4/Flag_of_the_United_States.svg',
-    area: 9629091,
-    population: 324459463,
-  },
-  {
-    name: 'India',
-    flag: '4/41/Flag_of_India.svg',
-    area: 3287263,
-    population: 1324171354,
-  },
-  {
-    name: 'Indonesia',
-    flag: '9/9f/Flag_of_Indonesia.svg',
-    area: 1910931,
-    population: 263991379,
-  },
-  {
-    name: 'Tuvalu',
-    flag: '3/38/Flag_of_Tuvalu.svg',
-    area: 26,
-    population: 11097,
-  },
-  {
-    name: 'China',
-    flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
-    area: 9596960,
-    population: 1409517397,
-  },
-];
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-table',
@@ -96,18 +8,34 @@ const COUNTRIES: Country[] = [
   styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
+  API_URL = 'https://api.coinlore.net/api/tickers/?start=100&limit=100';
+  objectKeys = Object.keys;
+  allCoins = [];
+  allCoinsData = [];
   page = 1;
-  pageSize = 4;
-  collectionSize = COUNTRIES.length;
+  pageSize = 20;
+  collectionSize = 100;
 
-  constructor() {}
+  constructor(private apiService: ApiService) {}
 
-  ngOnInit(): void {}
-
-  get countries(): Country[] {
-    return COUNTRIES.map((country, i) => ({ id: i + 1, ...country })).slice(
-      (this.page - 1) * this.pageSize,
-      (this.page - 1) * this.pageSize + this.pageSize
+  ngOnInit(): void {
+    this.apiService.sendGetRequest(this.API_URL).subscribe(
+      (data: any[]) => {
+        this.allCoins = data;
+        this.allCoinsData = this.allCoins['data'];
+        console.log(this.allCoins);
+        console.log(this.allCoinsData);
+      },
+      (error) => console.log(error)
     );
+  }
+
+  get coins(): any[] {
+    return this.allCoinsData
+      .map((coin, i) => ({ row_id: i + 1, ...coin }))
+      .slice(
+        (this.page - 1) * this.pageSize,
+        (this.page - 1) * this.pageSize + this.pageSize
+      );
   }
 }
