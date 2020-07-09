789152cfff40ad5fa379e22225f018ab2fd24175
Eszter <steszti@gmail.com>
2020-07-09 18:04:40 +0200

    refactor: async pipe

--- src/app/coin-details/coin-details.component.ts
+++ src/app/coin-details/coin-details.component.ts
@@ -1,34 +1,21 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../api.service';
-import { Subscription } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { TickerData } from '../interfaces/tickers.interfaces';
+import { switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-coin-details',
   templateUrl: './coin-details.component.html',
   styleUrls: ['./coin-details.component.css'],
 })
-export class CoinDetailsComponent implements OnInit, OnDestroy {
-  coinDetails: TickerData[] = [];
-  subscription: Subscription;
+export class CoinDetailsComponent {
+  coinDetails$ = this.route.params.pipe(
+    switchMap((params) =>
+      this.apiService
+        .sendGetRequestId(params.id)
+        .pipe(map((response) => response[0]))
+    )
+  );
 
   constructor(private route: ActivatedRoute, private apiService: ApiService) {}
-
-  ngOnInit(): void {
-    this.subscription = this.route.params
-      .pipe(switchMap((params) => this.apiService.sendGetRequestId(params.id)))
-      .subscribe(
-        (data) => {
-          this.coinDetails = data;
-          console.log(this.coinDetails);
-        },
-        (error) => console.log(error)
-      );
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
 }
