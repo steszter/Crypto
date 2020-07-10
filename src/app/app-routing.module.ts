8d1bf4b68f83dc85bcff81a8cff72b56d07e0648
Eszter <steszti@gmail.com>
2020-07-10 14:10:45 +0200

    style: app, navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CryptoDataComponent } from './crypto-data/crypto-data.component';
 import { CoinDetailsComponent } from './coin-details/coin-details.component';
+import { TableComponent } from './table/table.component';
 
 const routes: Routes = [
   { path: 'crypto', component: CryptoDataComponent },
+  { path: 'coins', component: TableComponent },
   { path: 'coin/:id', component: CoinDetailsComponent },
   { path: '', redirectTo: '/crypto', pathMatch: 'full' },
   { path: '**', redirectTo: '/crypto', pathMatch: 'full' },
